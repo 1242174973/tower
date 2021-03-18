da73fcd796a389b252a436f400c41e661819bc1c
梦屿千寻 <49826632+1242174973@users.noreply.github.com>
2021-03-18 11:47:44 +0800

    修改默认日志打印，添加后台项目

---
+++ tower-admin/src/main/java/com/tower/TowerAdminApplication.java
@@ -0,0 +1,18 @@
+package com.tower;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @author 梦-屿-千-寻
+ * @date2021/3/16 14:38
+ */
+@SpringBootApplication
+@MapperScan("com.tower.mapper")
+public class TowerAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TowerAdminApplication.class, args);
+    }
+}
--- tower-game/src/main/java/com/tower/handler/ControllerExceptionHandler.java
+++ tower-game/src/main/java/com/tower/handler/ControllerExceptionHandler.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+/**
+ * @author Administrator
+ */
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
