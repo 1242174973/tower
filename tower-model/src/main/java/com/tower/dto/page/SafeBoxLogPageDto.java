836f4199a5991b81b5937342e36e69da465101e2
梦屿千寻 <49826632+1242174973@users.noreply.github.com>
2021-03-19 10:20:25 +0800

    添加保险柜存取分记录接口

---
+++ tower-model/src/main/java/com/tower/dto/page/SafeBoxLogPageDto.java
@@ -0,0 +1,13 @@
+package com.tower.dto.page;
+
+import com.tower.dto.SafeBoxLogDto;
+import lombok.Data;
+
+/**
+ * @author 梦-屿-千-寻
+ * @date2021/3/18 14:34
+ */
+@Data
+public class SafeBoxLogPageDto extends PageDto<SafeBoxLogDto> {
+
+}
