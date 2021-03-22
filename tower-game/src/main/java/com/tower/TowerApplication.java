02544abb2536f2def41f8c051b77734d5d007da5
梦屿千寻 <49826632+1242174973@users.noreply.github.com>
2021-03-22 14:34:06 +0800

    添加mybatis-plus配置类

--- tower-game/src/main/java/com/tower/TowerApplication.java
+++ tower-game/src/main/java/com/tower/TowerApplication.java
@@ -1,6 +1,5 @@
 package com.tower;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,7 +9,6 @@ import org.springframework.context.ConfigurableApplicationContext;
  * @date2021/3/16 14:38
  */
 @SpringBootApplication
-@MapperScan("com.tower.mapper")
 public class TowerApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(TowerApplication.class, args);
---
+++ tower-game/src/main/java/com/tower/config/MybatisPlusConfig.java
@@ -0,0 +1,24 @@
+package com.tower.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author 梦-屿-千-寻
+ * @date 2021/3/22 10:37
+ */
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.tower.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        paginationInterceptor.setDbType(DbType.MYSQL);
+        return paginationInterceptor;
+    }
+}
